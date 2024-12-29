d2ffb0325b276cfee63a52c426fff2119f94c33b
Abderrahmane Fethi <fethiabderrahmane1@gmail.com>
2024-12-29 17:14:46 +0100

    in progress

--- keycodes-symphony.js
+++ keycodes-symphony.js
@@ -3,9 +3,19 @@ import { colors } from './fifty-shades-of-cold.data.js'
 export const compose = () => {
     document.addEventListener('keydown', (event) => {
         const div = document.createElement("div")
+        const randomColor = getRandomColor();
         div.setAttribute("class", "note")
-        div.style.backgroundColor = "orange"
+        div.style.backgroundColor = randomColor;
         div.textContent = (`${event.key}`)
         document.body.append(div)
     })
+}
+
+const getRandomColor = () => {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
 }
